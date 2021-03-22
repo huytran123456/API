80072c01b86baf2d103c137cf9f0015af94fb4de
LAPTOP-NP1GDI8O\hi <trandinhhuy1996tv@gmail.com>
2021-03-22 20:37:31 +0700

    first

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -70,7 +70,7 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(int $id)
     {
         //
         $SelectR=['first_name',
